9717ced80d5b47cc0b3553ddccbe2d3a5073640c
Mostafa Saber <mostafa.saber3789@gmail.com>
2018-03-02 16:27:55 +0200

    try on branch

--- src/main/java/com/ecommerce/sw2/Sw2Application.java
+++ src/main/java/com/ecommerce/sw2/Sw2Application.java
@@ -2,6 +2,7 @@ package com.ecommerce.sw2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,12 @@ public class Sw2Application {
 	public String HelloWorld()
 	{
 		return "Hello World";
+	}
+
+	@RequestMapping("/home/{num1}/{num2}")
+	public int sum(@PathVariable int num1 , @PathVariable int num2)
+	{
+		return num1 + num2;
 	}
 
 	public static void main(String[] args) {
